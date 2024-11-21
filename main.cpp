545d5732c827a6dae4530746dad0be204f2b8635
Max <maxstill2006@gmail.com>
2024-11-21 21:00:50 +0200

    +

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void calculateSumAndProduct(int* array, int size, int* sum, int* product) {
+    *sum = 0;
+    *product = 1;
+
+    for (int i = 0; i < size; ++i) {
+        *sum += array[i];
+        *product *= array[i];
+    }
+}
+
 int main() {
-	return 0;
+    int arr[] = { 1, 2, 3, 4 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = 0;
+    int product = 0;
+
+    calculateSumAndProduct(arr, size, &sum, &product);
+
+    cout << "Sum: " << sum << endl;
+    cout << "Mult: " << product << endl;
+
+    return 0;
 }
