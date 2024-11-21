fe0c5a6b221eb45171df2820ccd9390e99db1f43
Max <maxstill2006@gmail.com>
2024-11-21 21:14:09 +0200

    +

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void allocateArray(int*& arr, int size) {
-    arr = new int[size];
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
 }
 
-void initializeArray(int* arr, int size, int value) {
+int* removePrimes(int* arr, int& size) {
+    int newSize = 0;
     for (int i = 0; i < size; ++i) {
-        arr[i] = value;
+        if (!isPrime(arr[i])) {
+            ++newSize;
+        }
     }
+
+    int* newArr = new int[newSize];
+    int j = 0;
+    for (int i = 0; i < size; ++i) {
+        if (!isPrime(arr[i])) {
+            newArr[j++] = arr[i];
+        }
+    }
+
+    delete[] arr;
+    size = newSize;
+    return newArr;
 }
 
 void printArray(const int* arr, int size) {
@@ -18,69 +37,18 @@ void printArray(const int* arr, int size) {
     cout << endl;
 }
 
-void deleteArray(int*& arr) {
-    delete[] arr;
-    arr = nullptr;
-}
-
-void addElement(int*& arr, int& size, int value) {
-    int* newArr = new int[size + 1];
-    for (int i = 0; i < size; ++i) {
-        newArr[i] = arr[i];
-    }
-    newArr[size] = value;
-    delete[] arr;
-    arr = newArr;
-    ++size;
-}
-
-void insertElement(int*& arr, int& size, int index, int value) {
-    if (index < 0 || index > size) return;
-    int* newArr = new int[size + 1];
-    for (int i = 0; i < index; ++i) {
-        newArr[i] = arr[i];
-    }
-    newArr[index] = value;
-    for (int i = index; i < size; ++i) {
-        newArr[i + 1] = arr[i];
-    }
-    delete[] arr;
-    arr = newArr;
-    ++size;
-}
-
-void deleteElement(int*& arr, int& size, int index) {
-    if (index < 0 || index >= size) return;
-    int* newArr = new int[size - 1];
-    for (int i = 0; i < index; ++i) {
-        newArr[i] = arr[i];
-    }
-    for (int i = index + 1; i < size; ++i) {
-        newArr[i - 1] = arr[i];
-    }
-    delete[] arr;
-    arr = newArr;
-    --size;
-}
-
 int main() {
-    int* arr = nullptr;
-    int size = 0;
+    int* arr = new int[6] {2, 3, 4, 5, 6, 7};
+    int size = 6;
 
-    allocateArray(arr, 5);
-    initializeArray(arr, 5, 10);
-    printArray(arr, 5);
-
-    addElement(arr, size, 20);
+    cout << "Original array: ";
     printArray(arr, size);
 
-    insertElement(arr, size, 2, 30);
+    arr = removePrimes(arr, size);
+
+    cout << "Array after removing primes: ";
     printArray(arr, size);
 
-    deleteElement(arr, size, 3);
-    printArray(arr, size);
-
-    deleteArray(arr);
-
+    delete[] arr;
     return 0;
 }
